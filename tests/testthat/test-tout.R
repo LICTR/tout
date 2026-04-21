564a44af771231f33fe213b1a8860413ad7c9798
Duncan Wilson <meddwilb@leeds.ac.uk>
2026-04-21 16:12:51 +0100

    Skipping doppelganger tests on CI

--- tests/testthat/test-tout.R
+++ tests/testthat/test-tout.R
@@ -10,14 +10,16 @@ test_that("printing works (continuous)", {
 
 
 test_that("plotting works (binary)", {
+  skip_on_ci()
   x <- tout_design(rho_0=0.5, rho_1=0.7, alpha_nom=0.05, beta_nom=0.1, gamma_nom = 0.9)
   bin_fig <- function() plot(x)
-  vdiffr::expect_doppelganger("Binary tout plot", bin_fig, tolerance = 2)
+  vdiffr::expect_doppelganger("Binary tout plot", bin_fig)
 })
 
 test_that("plotting works (continuous)", {
+  skip_on_ci()
   x <- tout_design(rho_0=0, rho_1=0.4, alpha_nom=0.05, beta_nom=0.1, gamma_nom = 0.6, sigma=1)
   cont_fig <- function() plot(x)
-  vdiffr::expect_doppelganger("Continuous tout plot", cont_fig, tolerance = 2)
+  vdiffr::expect_doppelganger("Continuous tout plot", cont_fig)
 })
 
